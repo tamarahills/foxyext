2e51b945870a6107d8b584ebb87ba0ae44f97d63
Anna Chernysheva <annac@exaltcg.com>
2017-11-17 13:01:55 +0200

    Added bookmark feature, fixed height

---
+++ sidebar/bookmark/panelbookmark.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const info = document.querySelector('.info');
+  const title = document.querySelector('.title');
+  const url = document.querySelector('.url');
+
+  browser.windows.getLastFocused({ populate: true })
+    .then((logTabs) => {
+      const activeTab = logTabs.tabs.find(tab => tab.active);
+
+      browser.bookmarks
+        .search({ url: activeTab.url })
+        .then((bookmarkItems) => {
+          if (bookmarkItems.length) {
+            info.innerText = 'This URL is already present in your bookmarks';
+            return;
+          }
+          browser.bookmarks.create({
+            index: 0,
+            title: activeTab.title,
+            url: activeTab.url,
+          }).then(() => {
+            info.innerText = 'URL is added to your bookmarks';
+            title.innerText = activeTab.title;
+            url.innerText = activeTab.url;
+          }).catch((e) => {
+            console.log(e);
+          });
+        });
+    })
+    .catch(() => {
+      info.innerText = 'We cannot add this to bookmarks';
+    });
+});
